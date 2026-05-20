c2dc5b0f3532d247df9c00566e820f41d025d91b
MgkJosue <josue.alvarez9@hotmail.com>
2026-05-19 17:19:15 -0700

    New emergent links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/brunch.html",
+        destination: "/menu",
+        permanent: true,
+      },
+      {
+        source: "/lunch.html",
+        destination: "/menu",
+        permanent: true,
+      },
+      {
+        source: "/menu.html",
+        destination: "/menu",
+        permanent: true,
+      },
+      {
+        source: "/contact.html",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/index.html",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
